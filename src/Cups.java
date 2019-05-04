76c981968106cbf9c0ee5ba1436991b2b7ee1610
greenone092 <greenone092@gmail.com>
2019-05-04 13:30:18 +0800

    Cleanup
    -Removed Radius / Height from class Cups as they are only used and also not the only way to generate volumeCm3
    -Added user-friendly setters for most member variables
    -Added getters and multiple setters for VolumeCm3, turned it into private to reduce possible future errors.
    -Deleted Starbucks class for now due to difficulties in using it.
    -Deleted Coke Template for now due to difficulties in using them.
    -Added simplistic comparasion

--- src/Cups.java
+++ src/Cups.java
@@ -1,90 +1,112 @@
 import java.util.Scanner;
 
-public class Cups {
+public class Cups implements Cloneable  {
+	
 	double filling;
-	double lowerRadiusCm;
-	double upperRadiusCm;
-	double heightCm;
 	double price;
 	String name;
 	
 	private double volumeCm3;
-	
 
 	
-	public Cups(double Filling, double LowerRadiusCm, double UpperRadiusCm, double HeightCm, double Price, String Name)
+	public Cups(double Filling, double volumeCm3, double price, String Name)
 	{
 		this.filling = Filling;
-		this.lowerRadiusCm = LowerRadiusCm;
-		this.upperRadiusCm = UpperRadiusCm;
-		this.heightCm = HeightCm;
-		this.price = Price;
+		this.volumeCm3 = volumeCm3;
+		this.price = price;
 		this.name = Name;
 	}
 	
 	public Cups()
 	{}
 	
-	
 	public Cups(double price, String name)
 	{}
 	
-	public double volumeCm3()
+	public double getVolumeCm3()
 	{
-		if(volumeCm3 != 0)
-			return volumeCm3;
-		
-		return filling * Math.PI / 3 * heightCm *
+		return volumeCm3;
+	}
+	
+	public void setVolumeCm3(double volumeCm3)
+	{
+		this.volumeCm3 = volumeCm3;
+	}
+	
+	public void advancedSetVolumeCm3(double lowerRadiusCm, double upperRadiusCm, double heightCm)
+	{
+		volumeCm3 = filling * Math.PI / 3 * heightCm *
 				(Math.pow(lowerRadiusCm, 2) + lowerRadiusCm * upperRadiusCm + Math.pow(upperRadiusCm, 2));
 	}
 	
-	public double pricePerVolume()
+	public void ouncesSetVolumeCm3(double ounces)
 	{
-		return price / volumeCm3();
+		volumeCm3 = 29.5735D * ounces;
 	}
 	
-	public void printName()
+	public double getVolumePerPrice()
 	{
-		System.out.println(name);
+		return volumeCm3 / price;
 	}
 	
-	public void userInputName()
+	public void userInputDetails()
 	{
-		boolean finished = false;
-		do{
-			
-		System.out.println("Enter the name of the product");
+		userInputName(false);
+		userInputVolume(true, false);
+		userInputPrice(true, false);
+	}
+	
+	private void userInputName(boolean update)
+	{		
+		String type;
+		if(update == true)
+		type = "Update";
+		else
+		type = "Enter";
+		
+		System.out.println(type + " the name of the product");
 		Scanner keyboard = new Scanner(System.in);
 		String input = keyboard.nextLine();
 		
-			switch(input)
-			{
-			case "Coke Can":
-				name = "Coke Can";
-				volumeCm3 = 330;
-				userInputPrice();
-				finished = true;
-				break;
-				
-			default: 
-				System.out.println("No Match, Please Try Again"); 
-				break;
-			}
-		
-		} while (!finished);
-		
+		this.name = input;
 	}
 	
-	public void userInputPrice()
+	private void userInputPrice(boolean nameGiven, boolean update)
 	{
-		boolean finished = false;
-		do{
-			
-		System.out.println("Enter the price of the " + name);
+		String type;
+		if(update == true)
+		type = "Update";
+		else
+		type = "Enter";
+		
+		if(nameGiven == true)
+		System.out.println(type + " the price of the " + name);
+		else
+		System.out.println(type + " the price of the product");
+		
 		Scanner keyboard = new Scanner(System.in);
 		double input= keyboard.nextDouble();
 		
-		} while (!finished);
+		this.price = input;
+	}
+	
+	private void userInputVolume(boolean nameGiven, boolean update)
+	{
+		String type;
+		if(update == true)
+		type = "Update";
+		else
+		type = "Enter";
+		
+		if(nameGiven == true)
+		System.out.println(type + " the Volume of the " + name);
+		else
+		System.out.println(type + " the Volume of the product");
+		
+		Scanner keyboard = new Scanner(System.in);
+		double input= keyboard.nextDouble();
+		
+		setVolumeCm3(input);
 	}
 	
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,16 +1,29 @@
 import java.util.*;
+import java.lang.Math; 
 
 public class MyClass {
+	
+	
 	public static void main(String[] args) {	
 			
 			Cups firstProduct =  new Cups();
 			Cups secondProduct =  new Cups();
 			
-			firstProduct.userInputName();
-			secondProduct.userInputName();
+			System.out.println("Please enter the details of the first product for comparaion");
+			firstProduct.userInputDetails();
+
+			System.out.println("Please enter the details of the second product for comparaion");
+			secondProduct.userInputDetails();
 			
-			
-			
+			System.out.println("Here's the comparasion between them!");
+			String message;
+			double pricePerVolumeDiffPercent = (firstProduct.getVolumePerPrice() / secondProduct.getVolumePerPrice() - 1) * 100;
+			if(pricePerVolumeDiffPercent < 100)
+				message = "cheaper ";
+			else
+				message = "more expensive ";
+				
+			System.out.println("The " + firstProduct.name + " is " + Math.abs(pricePerVolumeDiffPercent) + "% " + message + "than the " + secondProduct.name);
 			
 	    }
 }
